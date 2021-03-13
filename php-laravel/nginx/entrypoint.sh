fd366134826344df9f20448b4dccc9c05f890ae0
xuyida.xyd <xuyida.xyd@alibaba-inc.com>
2021-03-13 11:45:51 +0800

    Introduce composer.json schema and refactor server starting order.

--- php-laravel/nginx/entrypoint.sh
+++ php-laravel/nginx/entrypoint.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 
-# Start PHP-FPM in background
-php-fpm -D
+startNginxWhenFpmUp() {
+    # Make sure port for NGINX proxying requests to PHP-FPM is up
+    RETRY_INTERVAL=0.2 # in seconds
+    while ! netstat -an | grep 'LISTEN\>' | grep ':9000\>'; do
+        sleep $RETRY_INTERVAL
+    done
 
-# Make sure port for NGINX proxying requests to PHP-FPM is up
-RETRY_INTERVAL=0.2 # in seconds
-while ! netstat -an | grep 'LISTEN\>' | grep ':9000\>'; do
-    sleep $RETRY_INTERVAL
-done
+    # Start NGINX server background 
+    nginx -g 'daemon on;'
+}
 
-# Start NGINX server in foreground
-nginx -g 'daemon off;'
+# In background, repeatedly check if PHP-FPM is up
+startNginxWhenFpmUp &
+
+# Start PHP-FPM in foreground
+php-fpm
